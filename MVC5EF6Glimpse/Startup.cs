0b44efcb3dd5a5c3c947cefc0230c7ff09315fa5
coopermoto <barrie@nouver.net>
2014-02-21 19:33:22 +0000

    Initial commit

---
+++ MVC5EF6Glimpse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5EF6Glimpse.Startup))]
+namespace MVC5EF6Glimpse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
